9c91c7bba53b374fadf51b2077614075da1ac3e3
Mathieu Laurent <mathieu.laurent@gmail.com>
2013-01-18 20:33:12 +0100

    bookmarklet countword display word count

--- countword/countword.js
+++ countword/countword.js
@@ -1,18 +1,6 @@
-javascript: (function(e, a, g, h, f, c, b, d) {
-	if(!(f = e.jQuery) || g > f.fn.jquery || h(f)) {
-		c = a.createElement("script");
-		c.type = "text/javascript";
-		c.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + g + "/jquery.min.js";
-		c.onload = c.onreadystatechange = function() {
-			if(!b && (!(d = this.readyState) || d == "loaded" || d == "complete")) {
-				h((f = e.jQuery).noConflict(1), b = 1);
-				f(c).remove()
-			}
-		};
-		a.documentElement.childNodes[0].appendChild(c)
-	}
-})(window, document, "1.8.3", function($, L) { 
-	/* YOUR JQUERY CODE GOES HERE */
-	var K = window.getSelection && String(window.getSelection());
-	console.log(K);
-});
+javascript: (function() {
+	s = window.getSelection && String(window.getSelection());
+	s = s.replace(/\s\s+/g,' ');
+	w = s.split(' ');
+	window.alert('Word count : ' + w.length);
+}());
